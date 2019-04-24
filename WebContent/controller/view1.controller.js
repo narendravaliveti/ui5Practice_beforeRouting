8f4143e0193ffec45f51cd3dbad1b15f117e05ca
narendravaliveti <narendra.valiveti9977@gmail.com>
2019-04-24 12:56:04 +0530

    i18n.properties and separate file for model are added and corresponding changes are made

--- WebContent/controller/view1.controller.js
+++ WebContent/controller/view1.controller.js
@@ -1,23 +1,27 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-    ],function (oController,oMessageToast,JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+    ],function (oController, oMessageToast, JSONModel, ResourceModel) {
                 return oController.extend("controller.view1", {
                     onInit() {
-                        var oData = {
-                            student : {
-                                name : "Narendra"
-                            }
-                        };
-                        var oModel = new JSONModel(oData);
+                        // data model
+                        var oModel = new JSONModel("JSON/model.json");
                         this.getView().setModel(oModel);
+                        // i18n model
+                        var i18nModel = new ResourceModel({
+                            bundleName: "i18n.i18n"
+                        });
+                        this.getView().setModel(i18nModel, "i18n");
                     },
                     onBtnPress() {
                         app.to(page2);
                     },
                     onBtnPress1(oController) {
+                        var oBundle = this.getView().getModel("i18n").getResourceBundle();
                         var oName = this.getView().getModel().getProperty("/student").name;
+                        var sMsg = oBundle.getText("msgToastBtnPrsMsg", [oName]);
                         oMessageToast.show("Hello "+oName);
                     }
                 });
